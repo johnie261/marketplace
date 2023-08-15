d9bebd2eb1ad70520f4e94b899d013f5a61c28b1
johnie261 <njorogejohn261@gmail.com>
2023-08-16 02:43:08 +0300

    Add a new function to create new token

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -15,7 +15,7 @@ contract NFTMarketplace is ERC721URIStorage {
   }
 
   using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;  //_tokenId variable has the most recent minted tokenID
+  Counters.Counter private _tokenIds;  //_tokenId keeps track of minted nfts
   Counters.Counter private _itemsSold; //keeps track of number of items sold
 
   address payable owner; //owner is the contract address that created the contract
@@ -36,11 +36,56 @@ contract NFTMarketplace is ERC721URIStorage {
     address owner,
     address seller,
     uint256 price,
-    bool currentlyListed,
+    bool currentlyListed
   );
 
   //a mapping that maps tokenId to token info, can be used to retrive details about tokenId
   mapping(uint256 => ListedToken) private idToListedToken;
+
+
+  //first time a token is created it is listed here
+  function createToken(string memory tokenURI, uint256 price)
+    public payable returns (uint) {
+      _tokenIds.increment(); //incrment tokenId counter
+      uint256 newTokenId = _tokenIds.current();
+
+      //Mint the nft with tokenId to the address who called createToken
+      _safeMint(msg.sender, newTokenId);
+    
+      //Map the tokenId to the tokenURI(IPFS url)
+      _setTokenURI(newTokenId, tokenURI);
+
+      //a function to update global variables and emit events
+      createListedToken(newTokenId, price);
+
+      return newTokenId;
+    }
+
+  function createListedToken(uint256 tokenId, uint256 price) private {
+    //make sure sender senr enought ETH to pay for listing
+    require(msg.value == listPrice, "Please sent the correct price");
+    require(price > 0, "Make sure price isnt negative");
+
+    //update mapping of tokenIf to toke details
+    idToListedToken[tokenId] = ListedToken(
+      tokenId,
+      payable(address(this)),
+      payable(msg.sender),
+      price,
+      true
+    );
+
+    _transfer(msg.sender, address(this), tokenId);
+
+    //Emit the event for successfull transfer
+    emit TokenListedSuccess(
+      tokenId, 
+      address(this), //owner
+      msg.sender, //seller
+      price, 
+      true
+    );
+  }
 
 
 }
