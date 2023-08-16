8a1985e88ea968315fdcfd733773af8edb23f3ae
johnie261 <njorogejohn261@gmail.com>
2023-08-16 04:25:09 +0300

    Modify script file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,13 +3,17 @@ const hre = require("hardhat");
 const fs = require("fs");
 
 async function main() {
+  // get the signer that we will use to deploy
   const [deployer] = await ethers.getSigners();
-  const balance = await deployer.getBalance();
+
+  //get the NFTMarketplace smart contract object and deploy it
   const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
   const marketplace = await Marketplace.deploy();
 
   await marketplace.deployed();
 
+  //pull the address and ABI out while you deploy, since that will be key in interacting
+  // with the smart contract
   const data = {
     address: marketplace.address,
     abi: JSON.parse(marketplace.interface.format('json'))
