1b5425dd6099bf7c4fffc4cdabb3abf8b3b337b8
johnie261 <njorogejohn261@gmail.com>
2023-08-17 10:35:55 +0300

    Add function to get NFTs of the logged in user

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,11 +4,59 @@ import MarketplaceJSON from "../Marketplace.json";
 import axios from "axios";
 import { useState } from "react";
 import NFTTile from "./NFTTile";
+import { ethers } from "ethers";
 
 export default function Profile () {
     const [data, updateData] = useState([]);
     const [address, updateAddress] = useState("0x");
     const [totalPrice, updateTotalPrice] = useState("0");
+    const [dataFetched, updateFetched] = useState(false);
+
+    const getNFTData = async(tokenId) => {
+      let sumPrice = 0;
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const addr = await signer.getAddress()
+
+      //Pull the deployed contract instance
+      let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
+
+      //create an NFT Token
+      let transaction = await contract.getMyNFTs()
+
+      //takes the metatadata from tokenuri and data returned by getmynfts()
+      //create an object of info to be displayed
+      const items = await Promise.all(transaction.map(async(i) => {
+        const tokenURI = await contract.tokenURI(i.tokenId);
+        let meta = await axios.get(tokenURI)
+        meta = meta.data;
+
+        let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.image,
+          name: meta.name,
+          description: meta.description,
+        }
+        sumPrice += Number(price);
+        return item;
+      }))
+
+      updateData(items)
+      updateFetched(true)
+      updateAddress(addr)
+      updateTotalPrice(sumPrice.toPrecision(3))
+    }
+
+    const params = useParams()
+    const tokenId = params.tokenId;
+    if(!dataFetched) {
+      getNFTData(tokenId)
+    }
     
     return (
         <div className="profileClass" style={{"min-height":"100vh"}}>
