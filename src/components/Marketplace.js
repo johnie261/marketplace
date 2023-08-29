52fd5cbdbf64ef1f498d3b52812f3c84a3d50178
johnie261 <njorogejohn261@gmail.com>
2023-08-29 15:54:26 +0300

    Add a loading effect

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import NFTTile from "./NFTTile";
 import MarketplaceJSON from "../Marketplace.json";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { GetIpfsUrlFromPinata } from "../utils";
 
 export default function Marketplace() {
@@ -40,26 +40,34 @@ async function getAllNFTs() {
 
     updateFetched(true);
     updateData(items);
+    setIsLoading(false);
 }
 
-if(!dataFetched)
-    getAllNFTs();
+useEffect(() => {
+    if (!dataFetched) {
+      setIsLoading(true);
+      getAllNFTs();
+    }
+  }, [dataFetched]);
 
-if(isLoading) {
-  <div>loading...</div>
-}
+// if(!dataFetched)
+//     getAllNFTs();
 
 return (
     <div>
-        <Navbar></Navbar>
-        <div className="flex flex-col place-items-center mt-20">
+        <Navbar />
+        <div className="flex flex-col place-items-center mt-20 h-screen">
             <div className="md:text-xl font-bold text-white">
                 Top NFTs
             </div>
             <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
-                {data.map((value, index) => {
-                    return <NFTTile data={value} key={index}></NFTTile>;
-                })}
+                {isLoading ? (
+                    <div>Loading...</div>
+                )
+                 : (
+                    data.map((value, index) => (
+                        <NFTTile data={value} key={index}></NFTTile>
+                )))}
             </div>
         </div>            
     </div>
